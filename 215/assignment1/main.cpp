7e5bcab992145ddd1ed224b5a32f72c86c2dcf09
METZ <bmetz@cocsce-l1d43-36.ds.sc.edu>
2018-09-10 01:10:49 -0400

    CSCE215 Files

---
+++ 215/assignment1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  char* command_line;
+  char buffer[55];
+
+  if(argc != 2){
+    cout << "usage: "<< argv[0] << " Some Value\n";
+    exit(0);
+  }
+  command_line=argv[1];
+  cout << "You entered  :" << command_line <<" from the command line." << endl;
+  sprintf(buffer, "./findname.sh %s" ,command_line);
+  system(buffer);
+}
